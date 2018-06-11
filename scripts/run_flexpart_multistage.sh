5eb911f3cc34a49fb80e54257238c15874f2be93
Quirin Pamp <pampq@cip.ifi.lmu.de>
2018-06-11 21:13:50 +0200

    Add container run script 0.1

---
+++ scripts/run_flexpart_multistage.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#convenience script for running the flexpart/multi_stage container.
+
+# constants:
+
+IMAGE_VERSION='0.1'
+IMAGE_NAME='flexpart/multi_stage'
+CONTAINER_NAME='flexpart-prototype'
+INPUT_VOLUME='flexpart_input_local'
+OUTPUT_VOLUME='flexpart_output_local'
+
+# commands:
+
+docker run\
+ --name ${CONTAINER_NAME}\
+ -it --entrypoint=bash\
+ --mount source=${INPUT_VOLUME},target=/flexpart_input\
+ --mount source=${OUTPUT_VOLUME},target=/flexpart_output\
+ ${IMAGE_NAME}:${IMAGE_VERSION}
