c3722d4a62999e5f66fcb30b42f89d37efae488d
Quirin Pamp <pampq@cip.ifi.lmu.de>
2018-06-26 11:20:34 +0200

    Add options processing to run script

--- scripts/run-flexpart-multi_stage.sh
+++ scripts/run-flexpart-multi_stage.sh
@@ -1,20 +1,44 @@
 #!/bin/bash
 #convenience script for running the flexpart/multi_stage container.
 
-# constants:
-
+#constants:
 IMAGE_VERSION='main'
 IMAGE_NAME='flexpart/multi_stage'
 CONTAINER_NAME='flexpart-prototype'
 INPUT_VOLUME='flexpart_input_local'
 OUTPUT_VOLUME='flexpart_output_local'
+HELP_TEXT="\
+./run-flexpart-multi_stage.sh <options>
 
-# commands:
+  -h    display this help message;
+  -i    run the docker container interactively;"
 
-docker run\
+
+#options processing:
+while getopts "ih" option "$@" ; do
+  case $option in
+    i) #run the container interactively:
+      EXTRA_OPTS='-it --entrypoint=bash'
+      ;;
+    h)
+      echo "$HELP_TEXT"
+      exit 0
+      ;;
+    \?)
+      echo "Invalid option -$OPTARG." >&2
+      exit 1
+      ;;
+    :)
+      echo "Option -$OPTARG requires an argument." >&2
+      exit 1
+      ;;
+  esac
+done
+
+#commands:
+docker run ${EXTRA_OPTS}\
  --name ${CONTAINER_NAME}\
  --rm\
- -it --entrypoint=bash\
  --mount source=${INPUT_VOLUME},target=/flexpart_input\
  --mount source=${OUTPUT_VOLUME},target=/flexpart_output\
  ${IMAGE_NAME}:${IMAGE_VERSION}
