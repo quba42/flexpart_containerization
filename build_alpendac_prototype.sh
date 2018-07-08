b8532d9c09b2393b7ec69671cf0e4351c78abbd8
Quirin Pamp <Q.Pamp.2009@my.bristol.ac.uk>
2018-07-08 12:36:48 +0200

    Adjust build script to reflect changed structure

--- build_alpendac_prototype.sh
+++ build_alpendac_prototype.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
-#Convenience script for building the flexpart/multi_stage container with proper
-#versioning.
+#Convenience script for building the flexpart/alpendac-prototype image with
+#fully explicit tag.
 
 #Constants:
 
 VERSION='main'
-NAME='flexpart/alpendac-prototype'
-SOURCE_DIR="../image_sources/${NAME}"
+BASE_NAME='flexpart'
+IMAGE_NAME='alpendac-prototype'
+FULL_NAME="${BASE_NAME}/${IMAGE_NAME}"
+SOURCE_DIR="alpendac_prototype"
 
 #Commands:
 
-docker build "${SOURCE_DIR}" -t "${NAME}:${VERSION}"
+docker build "${SOURCE_DIR}" -t "${FULL_NAME}:${VERSION}"
