e9eaff882319b831388bdb330912db1f3c0f0205
Quirin Pamp <Q.Pamp.2009@my.bristol.ac.uk>
2018-08-17 19:23:12 +0200

    Add the quicklook image sources

---
+++ quicklook/quicklook.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#Exit immediately if any command returns a non zero exit status:
+set -e
+
+#Use Agg backend instead of Xwindows to remove the need for a display:
+mkdir -p /root/.config/matplotlib
+touch /root/.config/matplotlib/matplotlibrc
+echo 'backend : Agg' > /root/.config/matplotlib/matplotlibrc
+
+#Execute the COMMAND directive:
+echo "$@"
+exec "$@"
