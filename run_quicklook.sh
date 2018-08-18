de4e351fd5d3f1296617896013402a1583e9436d
Quirin Pamp <Q.Pamp.2009@my.bristol.ac.uk>
2018-08-18 15:36:30 +0200

    Rework the quicklook run script

--- run_quicklook.sh
+++ run_quicklook.sh
@@ -6,15 +6,35 @@ IMAGE_VERSION='main'
 IMAGE_NAME='flexpart/quicklook'
 CONTAINER_NAME='quicklook-run'
 OUTPUT_VOLUME='flexpart_output_local'
+EXTRA_OPTS=''
+PASS_THROUGH_OPTS=''
 HELP_TEXT="\
 ./run-flexpart-alpendac-prototype.sh <options>
 
   -h    display this help message;
-  -i    run the docker container interactively;"
+  -i    run the docker container interactively;
+  -n <CONTAINER_NAME>
+        supply an alternate container name;
+  -o <FLEXPART_OUTPUT_FOLDER>
+        pass the flexpart output folder to the container;
+  -e    pass the -e argument to the container;
+  -v    pass the -v argument to the container;"
 
 #options processing:
-while getopts "ih" option "$@" ; do
+while getopts "ihn:o:ev" option "$@" ; do
   case $option in
+    v)
+      PASS_THROUGH_OPTS='-v'
+      ;;
+    e)
+      PASS_THROUGH_OPTS='-e'
+      ;;
+    o)
+      PASS_THROUGH_OPTS="${PASS_THROUGH_OPTS} -o ${OPTARG}"
+      ;;
+    n)
+      CONTAINER_NAME="${OPTARG}"
+      ;;
     i) #run the container interactively:
       EXTRA_OPTS='-it --entrypoint=bash'
       ;;
@@ -38,4 +58,5 @@ docker run ${EXTRA_OPTS}\
  --name ${CONTAINER_NAME}\
  --rm\
  --mount source=${OUTPUT_VOLUME},target=/flexpart_output\
- ${IMAGE_NAME}:${IMAGE_VERSION}
+ ${IMAGE_NAME}:${IMAGE_VERSION}\
+ ${PASS_THROUGH_OPTS}
